e6e96537dc31766804d02262b5b7a6b1266cdc8d
amallem <anirudh.mallem@rutgers.edu>
2015-10-03 17:58:57 -0400

    Added Main and MazeGenerator classes

---
+++ src/main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by anirudh on 10/3/15.
+ */
+public class main {
+
+    public static void main(String args[]){
+
+    }
+}
---
+++ src/utilities/MazeGenerator.java
@@ -0,0 +1,7 @@
+package utilities;
+
+/**
+ * Created by anirudh on 10/3/15.
+ */
+public class MazeGenerator {
+}
